cc8174192debea3860ebe221dabf686fa5504e1a
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-04-01 20:23:05 -0400

    tests written

---
+++ src/main/java/io/zipcoder/polymorphism/Snake.java
@@ -0,0 +1,8 @@
+package io.zipcoder.polymorphism;
+
+public class Snake extends Pet {
+
+    public Snake(String name) {
+        super(name);
+    }
+}
--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -1,7 +1,35 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 11/6/17.
  */
 public class MainApplicationTest {
+
+
+    @Test
+    public void listPetsTest() {
+        Integer numberOfPets = 3;
+        String[] petTypes = {"Dog", "Cat", "Snake"};
+        String[] petNames = {"Bruno", "Karma", "Elwood"};
+
+        String expected = "Dog Bruno\n Cat Karma\n Snake Elwood\n";
+        String actual = MainApplication.listPets(numberOfPets, petTypes, petNames);
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void petSpeakTest() {
+        Dog dog = new Dog("Bruno");
+        Cat cat = new Cat("Karma");
+        Snake snake = new Snake("Elwood");
+        Pet[] pets = {dog, cat, snake};
+
+        String expected = "Bruno the dog says woof\n Karma the cat says meow\n Elwood the snake says hiss";
+        String actual = MainApplication.petSpeak(pets);
+        Assert.assertEquals(expected, actual);
+
+    }
 }
